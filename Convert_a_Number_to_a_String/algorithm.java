dafbb4f3f2532c6815a7589439f8e8d727a81224
chaltteog <chaltteog@kakao.com>
2024-12-03 23:48:53 +0900

    feat: study codewars kata on "Convert a Number to a String!"

---
+++ Convert_a_Number_to_a_String/algorithm.java
@@ -0,0 +1,11 @@
+class Kata {
+    public static String numberToString(int num) {
+        return String.valueOf(num);
+    }
+}
+
+public class algorithm {
+    public static void main(String[] args) {
+        System.out.println(Kata.numberToString(123));
+    }
+}
